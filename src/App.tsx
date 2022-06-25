8b243684946bb9fd7043982b3d0f09725e552b4c
Uniquenik <postamentbox@bk.ru>
2022-06-25 17:24:29 +0300

    - small fixes - cleanup code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import {NewTodo} from "./components/newTodo/newTodo";
 import {TodoList} from "./components/todoList/todoList";
 import './scss/app-styles.scss'
